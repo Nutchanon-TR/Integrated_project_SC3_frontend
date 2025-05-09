c5cdce0299690028d0c69d7de9f6a3b02a7df8a5
Nutchanon-TR <nutchanon.charo@mail.kmutt.ac.th>
2025-05-09 15:31:01 +0700

    66115 add Check null inpud valid

--- src/components/BrandDropdown.vue
+++ src/components/BrandDropdown.vue
@@ -1,50 +1,87 @@
 <script setup>
-import { ref, onMounted } from 'vue'
-import { getAllData } from '@/libs/api'
+import { ref, onMounted,defineProps, watchEffect } from "vue";
+import { getAllData } from "@/libs/api";
 
-const isOpen = ref(false)
-const selected = ref('All Brands')
-const options = ref([])
+const prop = defineProps({
+  brandError: {
+    type: Boolean,
+  },
+});
 
-const URL = import.meta.env.VITE_ROOT_API_URL
+const errorColor = ref("border-gray-300");
+
+watchEffect(() => {
+  if(prop.brandError) {
+    errorColor.value = "border-red-300";
+  } else {
+    errorColor.value = "border-gray-300";
+  }
+});
+
+const isOpen = ref(false);
+const selected = ref("All Brands");
+const options = ref([]);
+
+const URL = import.meta.env.VITE_ROOT_API_URL;
 
 onMounted(async () => {
   try {
-    const data = await getAllData(`${URL}/itb-mshop/v1/brands`)
-    options.value = data.map(brand => brand.name)
-    console.log('Brands loaded:', options.value)
+    const data = await getAllData(`${URL}/itb-mshop/v1/brands`);
+    options.value = data.map((brand) => brand.name);
+    console.log("Brands loaded:", options.value);
+    console.log("load brand input: ",prop.brandError);
   } catch (error) {
-    console.error('โหลดข้อมูลแบรนด์ไม่สำเร็จ:', error.message)
+    console.error("โหลดข้อมูลแบรนด์ไม่สำเร็จ:", error.message);
   }
-})
+});
 </script>
 
 <template>
-    <div class="relative inline-block text-left">
-      <!-- ปุ่มหลัก -->
-      <button @click="isOpen = !isOpen" type="button"
-        class="inline-flex justify-between items-center w-48 px-4 py-2 bg-white border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 hover:bg-gray-50">
-        {{ selected }}
-        <svg class="-mr-1 ml-2 h-5 w-5" xmlns="http://www.w3.org/2000/svg" fill="none"
-          viewBox="0 0 24 24" stroke="currentColor">
-          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-            d="M19 9l-7 7-7-7" />
-        </svg>
-      </button>
-  
-      <!-- ตัวเลือก dropdown -->
-      <div v-if="isOpen"
-        class="origin-top-right absolute z-10 mt-2 w-48 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
-        <div class="py-1 max-h-60 overflow-y-auto">
-          <button
-            v-for="option in options"
-            :key="option"
-            @click="selected = option; isOpen = false"
-            class="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-blue-100 hover:text-blue-700">
-            {{ option }}
-          </button>
-        </div>
+  <div class="relative inline-block text-left">
+    <!-- ปุ่มหลัก -->
+    <button
+      @click="isOpen = !isOpen"
+      type="button"
+      :class="[
+        'inline-flex justify-between items-center w-48 px-4 py-2 bg-white border rounded-md shadow-sm text-sm font-medium text-gray-700 hover:bg-gray-50',
+        errorColor,
+      ]"
+    >
+      {{ selected }}
+      <svg
+        class="-mr-1 ml-2 h-5 w-5"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M19 9l-7 7-7-7"
+        />
+      </svg>
+    </button>
+
+    <!-- ตัวเลือก dropdown -->
+    <div
+      v-if="isOpen"
+      class="origin-top-right absolute z-10 mt-2 w-48 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5"
+    >
+      <div class="py-1 max-h-60 overflow-y-auto">
+        <button
+          v-for="option in options"
+          :key="option"
+          @click="
+            selected = option;
+            isOpen = false;
+          "
+          class="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-blue-100 hover:text-blue-700"
+        >
+          {{ option }}
+        </button>
       </div>
     </div>
-  </template>
-  
+  </div>
+</template>
--- src/components/ProductCreateAndEdit.vue
+++ src/components/ProductCreateAndEdit.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, reactive, onMounted, watchEffect } from "vue";
+import { ref, reactive, onMounted, watchEffect, defineProps } from "vue";
 import { addData, updateData } from "./../libs/api.js";
 import BlogProductCreateAndEdit from "./../components/BlogProductCreateAndEdit.vue";
 import BrandDropdown from "./BrandDropdown.vue";
@@ -7,6 +7,14 @@ const VITE_ROOT_API_URL = import.meta.env.VITE_ROOT_API_URL;
 
 const boxTextTailwind =
   "w-full p-2 border border-blue-300 rounded-md focus:ring-2 focus:ring-blue-500 focus:outline-none bg-blue-50";
+const boxTextTailwindError =
+  "w-full p-2 border border-red-300 rounded-md focus:ring-2 focus:ring-blue-500 focus:outline-none bg-blue-50";
+let brandError = ref(false);
+
+let boxTextTailwindModel = ref(boxTextTailwind);
+let boxTextTailwindPrice = ref(boxTextTailwind);
+let boxTextTailwindQuantity = ref(boxTextTailwind);
+let boxTextTailwindDesc = ref(boxTextTailwind);
 
 let product = reactive({
   model: "",
@@ -24,8 +32,48 @@ let product = reactive({
 });
 
 const saveProduct = async () => {
-  console.log("Product saved:", product);
-  await addData(VITE_ROOT_API_URL + `/itb-mshop/v1/sale-items`, product);
+  boxTextTailwindModel.value= boxTextTailwind;
+  boxTextTailwindPrice.value= boxTextTailwind;
+  boxTextTailwindQuantity.value= boxTextTailwind;
+  boxTextTailwindDesc.value= boxTextTailwind;
+  brandError.value = false;
+  if (product.brand.id == null || product.brand.name == "") {
+    console.log("Brand is not selected");
+    console.log(brandError.value);
+    brandError.value = true;
+  }
+  if (product.model == "") {
+    boxTextTailwindModel.value = boxTextTailwindError;
+    console.log(boxTextTailwindModel);
+  }
+  if (product.price == null) {
+    boxTextTailwindPrice.value = boxTextTailwindError;
+  }
+  if (product.quantity == null) {
+    boxTextTailwindQuantity.value = boxTextTailwindError;
+  }
+  if (product.description == "") {
+    boxTextTailwindDesc.value = boxTextTailwindError;
+  } else if (
+    product.brand.id == null ||
+    product.brand.name == "" ||
+    product.model == "" ||
+    product.price == null ||
+    product.quantity == null ||
+    product.description == ""
+  ) {
+    return;
+  } else if (product.id) {
+    console.log("Product updated:", product);
+    await updateData(
+      VITE_ROOT_API_URL + `/itb-mshop/v1/sale-items`,
+      product.id,
+      product
+    );
+  } else {
+    console.log("Product saved:", product);
+    await addData(VITE_ROOT_API_URL + `/itb-mshop/v1/sale-items`, product);
+  }
 };
 
 onMounted(() => {
@@ -55,82 +103,120 @@ watchEffect(() => {
 });
 </script>
 <template>
-  <div class="max-w-2xl mx-auto p-6 bg-white rounded-lg shadow-lg border border-blue-200">
+  <div
+    class="max-w-2xl mx-auto p-6 bg-white rounded-lg shadow-lg border border-blue-200"
+  >
     <h2 class="text-2xl font-bold text-blue-700 mb-6 text-center">
       Phone Details
     </h2>
     <div class="space-y-4">
       <div class="flex flex-col sm:flex-row sm:items-center sm:space-x-4 mb-4">
-        <label for="brand" class="font-medium text-gray-700 sm:w-32 mb-1 sm:mb-0">
+        <label
+          for="brand"
+          class="font-medium text-gray-700 sm:w-32 mb-1 sm:mb-0"
+        >
           Brand
         </label>
         <div class="w-full sm:w-auto">
-          <BrandDropdown />
+          <BrandDropdown :brandError="brandError" />
         </div>
       </div>
-
 
       <BlogProductCreateAndEdit>
         <template #text> Model </template>
         <template #inputText>
-          <input type="text" v-model="product.model" :class="`itbms-model ${boxTextTailwind}`" />
+          <input
+            type="text"
+            v-model="product.model"
+            :class="`itbms-model ${boxTextTailwindModel}`"
+          />
         </template>
       </BlogProductCreateAndEdit>
 
       <BlogProductCreateAndEdit>
         <template #text> Price (Baht) </template>
         <template #inputText>
-          <input type="number" v-model="product.price" :class="`itbms-price ${boxTextTailwind}`" /></template>
+          <input
+            type="number"
+            v-model="product.price"
+            :class="`itbms-price ${boxTextTailwindPrice}`"
+        /></template>
       </BlogProductCreateAndEdit>
 
       <BlogProductCreateAndEdit>
         <template #text> Description </template>
         <template #inputText>
-          <textarea v-model="product.description" rows="4" :class="`itbms-description ${boxTextTailwind}`"></textarea>
+          <textarea
+            v-model="product.description"
+            rows="4"
+            :class="`itbms-description ${boxTextTailwindDesc}`"
+          ></textarea>
         </template>
       </BlogProductCreateAndEdit>
 
       <BlogProductCreateAndEdit>
         <template #text> RAM (GB) </template>
         <template #inputText>
-          <input type="number" v-model="product.ramGb" :class="`itbms-ramGb ${boxTextTailwind}`" /></template>
+          <input
+            type="number"
+            v-model="product.ramGb"
+            :class="`itbms-ramGb ${boxTextTailwind}`"
+        /></template>
       </BlogProductCreateAndEdit>
 
       <BlogProductCreateAndEdit>
         <template #text> Screen Size (Inches) </template>
         <template #inputText>
-          <input type="number" v-model="product.screenSizeInch" step="0.1"
-            :class="`itbms-screenSizeInch ${boxTextTailwind}`" />
+          <input
+            type="number"
+            v-model="product.screenSizeInch"
+            step="0.1"
+            :class="`itbms-screenSizeInch ${boxTextTailwind}`"
+          />
         </template>
       </BlogProductCreateAndEdit>
 
       <BlogProductCreateAndEdit>
         <template #text> Storage (GB) </template>
         <template #inputText>
-          <input type="number" v-model="product.storageGb" :class="`itbms-storageGb ${boxTextTailwind}`" />
+          <input
+            type="number"
+            v-model="product.storageGb"
+            :class="`itbms-storageGb ${boxTextTailwind}`"
+          />
         </template>
       </BlogProductCreateAndEdit>
 
       <BlogProductCreateAndEdit>
         <template #text> Color </template>
         <template #inputText>
-          <input type="text" v-model="product.color" :class="`itbms-color ${boxTextTailwind}`" />
+          <input
+            type="text"
+            v-model="product.color"
+            :class="`itbms-color ${boxTextTailwind}`"
+          />
         </template>
       </BlogProductCreateAndEdit>
 
       <BlogProductCreateAndEdit>
         <template #text> Quantity </template>
         <template #inputText>
-          <input type="number" v-model="product.quantity" :class="`itbms-quantity ${boxTextTailwind}`" />
+          <input
+            type="number"
+            v-model="product.quantity"
+            :class="`itbms-quantity ${boxTextTailwindQuantity}`"
+          />
         </template>
       </BlogProductCreateAndEdit>
     </div>
 
     <!-- Submit Button -->
     <div class="mt-8 flex justify-end">
-      <button type="submit"
+      <button
+        type="submit"
         class="px-6 py-2 bg-blue-600 hover:bg-blue-700 text-white font-medium rounded-md transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
-        @click="saveProduct">
+        @click="saveProduct"
+      >
         Save
       </button>
     </div>
