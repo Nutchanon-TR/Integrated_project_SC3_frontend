cca47427a0a0c212ba5a5ce2127e1047f3325c37
Nutchanon-TR <nutchanon.charo@mail.kmutt.ac.th>
2025-05-18 17:44:15 +0700

    66115 try nginx

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import ProductList from "../views/ProductList.vue"
 import HomePage from '@/views/HomePage.vue'
 import ProductDetail from '@/views/ProductDetail.vue'
@@ -11,7 +11,7 @@ import BrandManage from '@/views/BrandManage.vue'
 
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
